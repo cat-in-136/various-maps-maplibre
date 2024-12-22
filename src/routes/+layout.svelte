983230da2a3dc2337ca62cca5153e4b26cbd1397
cat_in_136 <cat.in.136+github@gmail.com>
2024-12-22 19:49:17 +0900

    add route and layouts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	//import Header from './Header.svelte';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-Hello, world.
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Various Maps Vector Edition</title>
+</svelte:head>
+
+<style>
+</style>
