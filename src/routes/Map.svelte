832902efe1e5dd485702a00f88fbf449f175e7cd
cat_in_136 <cat.in.136+github@gmail.com>
2024-12-22 20:52:24 +0900

    Add Maplibre Geocoder integration and some compoenents

--- src/routes/Map.svelte
+++ src/routes/Map.svelte
@@ -1,9 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
+	// maplibre-gl
 	import maplibregl from 'maplibre-gl';
 	import 'maplibre-gl/dist/maplibre-gl.css';
+	// maplibre-gl-geocoder
+	import MaplibreGeocoder from '@maplibre/maplibre-gl-geocoder';
+	import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
 
-	onMount(() => {
+	const initMap = () => {
 		const map = new maplibregl.Map({
 			container: 'map',
 			center: [138.75, 36],
@@ -12,15 +16,81 @@
 			style: 'https://gsi-cyberjapan.github.io/gsivectortile-mapbox-gl-js/blank.json'
 		});
 
+		// Navigation (Zoom) control
 		map.addControl(
 			new maplibregl.NavigationControl({
 				visualizePitch: true,
 				showZoom: true,
 				showCompass: true
 			}),
-			'top-left'
+			'bottom-right'
 		);
-	});
+
+		// Scale control
+		map.addControl(
+			new maplibregl.ScaleControl({
+				maxWidth: 200,
+				unit: 'metric'
+			})
+		);
+
+		// Geocoder control
+		// https://maplibre.org/maplibre-gl-js/docs/examples/geocoder/
+		const geocoderApi = {
+			forwardGeocode: async (config) => {
+				const features = [];
+				try {
+					const request = `https://nominatim.openstreetmap.org/search?q=${
+						config.query
+					}&format=geojson&polygon_geojson=1&addressdetails=1`;
+					const response = await fetch(request);
+					const geojson = await response.json();
+					for (const feature of geojson.features) {
+						const center = [
+							feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+							feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2
+						];
+						const point = {
+							type: 'Feature',
+							geometry: {
+								type: 'Point',
+								coordinates: center
+							},
+							place_name: feature.properties.display_name,
+							properties: feature.properties,
+							text: feature.properties.display_name,
+							place_type: ['place'],
+							center
+						};
+						features.push(point);
+					}
+				} catch (e) {
+					console.error(`Failed to forwardGeocode with error: ${e}`);
+				}
+
+				return {
+					features
+				};
+			}
+		};
+		map.addControl(
+			new MaplibreGeocoder(geocoderApi, {
+				zoom: 14
+			})
+		);
+		// Geolocate control
+		map.addControl(
+			new maplibregl.GeolocateControl({
+				positionOptions: {
+					enableHighAccuracy: false
+				},
+				fitBoundsOptions: { maxZoom: 18 },
+				trackUserLocation: true,
+				showUserLocation: true
+			})
+		);
+	};
+	onMount(initMap);
 </script>
 
 <div id="map"></div>
