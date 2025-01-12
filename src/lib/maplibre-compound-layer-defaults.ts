e57484ad290d2ba5850d7654f3957f457dbe87d2
cat_in_136 <cat.in.136+github@gmail.com>
2025-01-12 12:55:40 +0900

    add 地理院地図/その他の地図
    - 写真+注記
    - 標準+陰影
    - 淡色+陰影
    - 軽い標準地図
    - 鉄道路線図
    - 地形だけ地図
    - 川だけ地図

--- src/lib/maplibre-compound-layer-defaults.ts
+++ src/lib/maplibre-compound-layer-defaults.ts
@@ -1,5 +1,13 @@
 import type * as MaplibreCompondLayerUI from './maplibre-compound-layer-ui';
 
+import gsi_ort_label from '../../static/assets/map-data/gsi-ort-label.min.json?url';
+import gsi_h_std from '../../static/assets/map-data/gsi-h-std.min.json?url';
+import gsi_h_pale from '../../static/assets/map-data/gsi-h-pale.min.json?url';
+import gsi_light from '../../static/assets/map-data/gsi-light.min.json?url';
+import gsi_railway from '../../static/assets/map-data/gsi-railway.min.json?url';
+import gsi_land from '../../static/assets/map-data/gsi-land.min.json?url';
+import gsi_river from '../../static/assets/map-data/gsi-river.min.json?url';
+
 export const BASE_LAYER_DEFAULT: MaplibreCompondLayerUI.LayerConfig.LayerConfigEntry[] = [
 	{
 		type: 'LayerGroup',
@@ -115,6 +123,61 @@ export const BASE_LAYER_DEFAULT: MaplibreCompondLayerUI.LayerConfig.LayerConfigE
 				title: '白地図',
 				url: 'https://gsi-cyberjapan.github.io/gsivectortile-mapbox-gl-js/blank.json',
 				maxNativeZoom: 17.9
+			},
+			{
+				type: 'LayerGroup',
+				title: 'その他の地図',
+				entries: [
+					{
+						type: 'Layer',
+						id: 'extra-gsi-ort-label',
+						title: '写真+注記',
+						url: gsi_ort_label,
+						maxNativeZoom: 17.9
+					},
+					{
+						type: 'Layer',
+						id: 'extra-gsi-h-std',
+						title: '標準+陰影',
+						url: gsi_h_std,
+						maxNativeZoom: 17.9
+					},
+					{
+						type: 'Layer',
+						id: 'extra-gsi-h-pale',
+						title: '淡色+陰影',
+						url: gsi_h_pale,
+						maxNativeZoom: 17.9
+					},
+					{
+						type: 'Layer',
+						id: 'extra-gsi-light',
+						title: '軽い標準地図',
+						url: gsi_light,
+						maxNativeZoom: 17.9
+					},
+					{
+						type: 'Layer',
+						id: 'extra-gsi-railway',
+						title: '鉄道路線図',
+						url: gsi_railway,
+						maxNativeZoom: 17.9
+					},
+					{
+						type: 'Layer',
+						id: 'extra-gsi-land',
+						title: '地形だけ地図',
+						url: gsi_land,
+						maxNativeZoom: 17.9
+					},
+					{
+						type: 'Layer',
+						id: 'extra-gsi-river',
+						title: '川だけ地図',
+						url: gsi_river,
+						maxNativeZoom: 17.9
+					}
+				]
 			}
 		]
 	},
