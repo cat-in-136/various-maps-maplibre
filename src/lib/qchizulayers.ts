77ab8dfea631f4e006244a157f316fda3bf3aafc
cat_in_136 <cat.in.136+github@gmail.com>
2025-09-18 00:44:41 +0900

    Fix filtering condition of QChizuLayers

--- src/lib/qchizulayers.ts
+++ src/lib/qchizulayers.ts
@@ -19,18 +19,31 @@ export class QChizuLayers extends GSIMapLayers {
 				if (!layerGroup.entries || layerGroup.entries?.length === 0) {
 					return undefined;
 				}
+				if ('src' in layerGroup) {
+					return undefined; // external reference currently unsupport.
+				}
 				if (layerGroup.title === '今昔マップ【谷謙二氏】') {
 					return undefined; // 今昔マップはすでにロードされているので除外
 				}
 			} else if (v?.type === 'Layer') {
 				const layer = v as MaplibreCompondLayerUI.LayerConfig.Layer;
-				if (layer.url.endsWith('.json')) {
+				if (
+					layer.url.endsWith('.pm.json') || // pmtiles
+					/{-[xyz]}/.test(layer.url) || // TMS tiles
+					layer.url.includes('/optimal_bvmap/') || // 国土地理院最適化ベクトルタイル
+					layer.url.startsWith('https://mapdata.qchizu.xyz/') || // CORS not allowed
+					layer.url.startsWith('http://gis.biodic.go.jp/') || // CORS not allowed
+					layer.url === './layers_json/54_ksj_kokuyurinya_2018_01.json' // pmtiles
+				) {
 					return undefined;
-				}
-				if (!layer.url.startsWith('https://')) {
+				} else if (layer.url.startsWith('./layers_json/')) {
+					layer.url = `https://qchizu.github.io/qchizu/${layer.url.substring(2)}`;
+				} else if (layer.url.startsWith('http://')) {
+					if (location.protocol == 'https:') {
+						return undefined; // access to http: from https: not allowed
+					}
+				} else if (!layer.url.startsWith('https://')) {
 					layer.url = new URL(layer.url, QCHIZULAYERS[0].url).href;
-				} else if (layer.url.startsWith('./layers_json')) {
-					layer.url = new URL(`../${layer.url}`, QCHIZULAYERS[0].url).href;
 				}
 				if (!layer.id) {
 					const rand = Math.random().toString(32).substring(2);
